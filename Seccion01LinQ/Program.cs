35116ef1b7516bef963163134351570267519da9
dgmrtabu <dgmr.tabu@gmail.com>
2022-04-27 10:06:15 -0400

    59 Filtros Numeros

--- Seccion01LinQ/Program.cs
+++ Seccion01LinQ/Program.cs
@@ -11,40 +11,82 @@ namespace Seccion01LinQ
     {
         static void Main(string [] args)
         {
-            OrdenNumeros();
-            OrdenNombres();
+            //OrdenNumeros();
+            //OrdenNombres();
+            FiltroNumeros();
             Console.Read();
         }
 
-        private static void OrdenNombres()
-        {
-            var lista = Utilidades.ObtenerListaNombres();
-            Console.WriteLine(string.Join(" - ", lista));
+        //private static void OrdenNombres()
+        //{
+        //    var lista = Utilidades.ObtenerListaNombres();
+        //    Console.WriteLine(string.Join(" - ", lista));
 
-            var listaAsc = lista.OrderBy(x => x).ToList();
-            Console.WriteLine(string.Join(" - ", listaAsc));
+        //    var listaAsc = lista.OrderBy(x => x).ToList();
+        //    Console.WriteLine(string.Join(" - ", listaAsc));
 
-            var listaDes = lista.OrderByDescending(x => x).ToList();
-            Console.WriteLine(string.Join(" - ", listaDes));
+        //    var listaDes = lista.OrderByDescending(x => x).ToList();
+        //    Console.WriteLine(string.Join(" - ", listaDes));
 
-            var listaReversada = lista.AsEnumerable().Reverse().ToList();
-            Console.WriteLine(string.Join(" - ", listaReversada));
+        //    var listaReversada = lista.AsEnumerable().Reverse().ToList();
+        //    Console.WriteLine(string.Join(" - ", listaReversada));
 
-        }
+        //}
 
-        private static void OrdenNumeros()
+        //private static void OrdenNumeros()
+        //{
+        //    var lista = Utilidades.ObtenerListaNumeros();
+        //    Console.WriteLine(string.Join(" - ", lista));
+
+        //    var listaAsc = lista.OrderBy(x => x).ToList();
+        //    Console.WriteLine(string.Join(" - ", listaAsc));
+
+        //    var listaDes = lista.OrderByDescending(x => x).ToList();
+        //    Console.WriteLine(string.Join(" - ", listaDes));
+
+        //    var listaReversada = lista.AsEnumerable().Reverse().ToList();
+        //    Console.WriteLine(string.Join(" - ", listaReversada));
+
+
+        //}
+
+        private static void FiltroNumeros()
         {
             var lista = Utilidades.ObtenerListaNumeros();
+            Console.WriteLine("Lista Original");
             Console.WriteLine(string.Join(" - ", lista));
 
-            var listaAsc = lista.OrderBy(x => x).ToList();
-            Console.WriteLine(string.Join(" - ", listaAsc));
+            Console.WriteLine($"Todos los elementos son mayores a 25:{ lista.All(x => x > 25)}");
+            Console.WriteLine($"Todos los elementos son mayores a 0:{ lista.All(x => x > 0)}");
 
-            var listaDes = lista.OrderByDescending(x => x).ToList();
-            Console.WriteLine(string.Join(" - ", listaDes));
+            Console.WriteLine($"Al menos un elemento es mayor a 100:{ lista.Any(x => x > 100)}");
+            Console.WriteLine($"Al menos un elemento es mayor a 10000:{ lista.Any(x => x > 10000)}");
 
-            var listaReversada = lista.AsEnumerable().Reverse().ToList();
-            Console.WriteLine(string.Join(" - ", listaReversada));
+            Console.WriteLine($"La lista contiene el elemento 25: {lista.Contains(25)}");
+            Console.WriteLine($"La lista contiene el elemento 55: {lista.Contains(55)}");
+
+            var listaMayor10 = lista
+                                    .Where(x => x > 10)
+                                    .ToList();
+            Console.WriteLine("Lista Mayor a 10");
+            Console.WriteLine(string.Join(" - ", listaMayor10));
+
+            Console.WriteLine();
+
+            var listaNumeros = new List<int> { 5, 47, 65, 1000 };
+
+            var lista2 = lista
+                            .Where(x => listaNumeros.Contains(x))
+                            .ToList();
+            Console.WriteLine("Lista existente");
+            Console.WriteLine(string.Join(" - ", lista2));
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Existen elemntos comunes entre las 2 listas: {lista.Any(x => listaNumeros.Contains(x))}");
+            Console.WriteLine($"Existen elemntos comunes entre las 2 listas: {lista.Any(x => new List<int> { 47, 65 }.Contains(x))}");
+
+            Console.WriteLine();
 
 
         }
